33c62d56d6560a7f96e8d5e9c286443dba287fbd
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-02-26 13:21:18 +0100

    added card class

--- CardGame.java
+++ CardGame.java
@@ -4,8 +4,16 @@ import java.util.Collections;
 import java.util.List;
 
 public class CardGame {
-
     public void main(String[] args) {
 
+    }
+
+    // This class represents a card
+    // Each card has it's own symbol, and a value
+
+    static class Card {
+        String color; // Card symbol
+        String rank; // Card name, 1, 2, 3, Q, K, J etc. 
+        int value; // Card number value used to decide who has the highest card
     }
 }
